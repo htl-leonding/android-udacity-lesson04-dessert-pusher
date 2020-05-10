25ff2d61a8bf44789f032e7382721956ecb2f938
htl-leonding <aenderemich12345>
2020-05-10 10:26:54 +0200

    L04-E14 Dummy-Lifecycle-Method in DessertTimer

--- app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
+++ app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
@@ -80,7 +80,7 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
             onDessertClicked()
         }
 
-        dessertTimer = DessertTimer()
+        dessertTimer = DessertTimer(this.lifecycle)
 
         // Set the TextViews to the right values
         binding.revenue = revenue
